ac1a8f3687e55a010a72b36cbca4ed34b12c813e
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-17 20:46:55 -0300

    first commit

---
+++ src/test/java/br/com/orcamento/controle/ControleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.orcamento.controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
