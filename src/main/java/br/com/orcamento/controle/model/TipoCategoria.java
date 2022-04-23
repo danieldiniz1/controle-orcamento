1e2d7cc610501a8073c8bd1de4e9f833e50e7a73
Daniel Diniz <daniel.diniz1@icloud.com>
2022-04-22 23:02:48 -0300

    populando Enum de categoria com as opçoes do negócio

--- src/main/java/br/com/orcamento/controle/model/TipoCategoria.java
+++ src/main/java/br/com/orcamento/controle/model/TipoCategoria.java
@@ -2,7 +2,14 @@ package br.com.orcamento.controle.model;
 
 public enum TipoCategoria {
 
-    Alimentacao(1, "Categoria de Alimentação como Restaurantes, bares, mercado");
+    OUTRAS(1,"Valor Default"),
+    ALIMENTACAO(2, "Gastos com Alimentação como Restaurantes, bares, mercado"),
+    SAUDE(3,"Gastos com Saude como farmacia, medico"),
+    MORADIA(4,"Gatsos com Moradia como alugel, financiamento, juros"),
+    TRANSPORTE(5,"Gastos com Trasnporte como onibus, combustivel, seguro"),
+    EDUCACAO(6,"Gastos com Educação, como escola, faculdade, cursos "),
+    LAZER(7,"Gastos com Lazer como festas, lanches e saidas"),
+    IMPREVISTOS(8,"Gastos com Imprevistos como acidentes");
 
     private Integer codigo;
     private String descricao;
